830664fe9c31eb5fa8c14645ca7cf3431ff00674
Samir Ziouche <ziouche.samir@gmail.com>
2016-04-19 15:59:35 +0200

    Ajout du sql

---
+++ MySQL.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE IF NOT EXISTS `chaine` (
+  `chaine_id` int NOT NULL AUTO_INCREMENT,
+  `prenom` varchar(20) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `age` char(2) NOT NULL,
+  `adresse` varchar(50) NOT NULL,	
+    PRIMARY KEY (`chaine_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
